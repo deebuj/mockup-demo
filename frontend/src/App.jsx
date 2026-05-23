b2852fa2d7a330ed0fd90507d1e09650f6bbaa34
deebujacob <>
2026-05-23 08:50:20 -0500

    added react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'sans-serif' }}>
+      <h1>Hello, World!</h1>
+      <p>A simple React app deployed with GitHub Actions</p>
+    </div>
+  )
+}
+
+export default App
